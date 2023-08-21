e19eb1d1618667a46d0aab88f545b73a6c33c9e6
Erick Tut <87912114+Erick-Tut@users.noreply.github.com>
2023-08-21 14:31:59 -0600

    8.15.23

---
+++ EJEMPLOS/8.15.23 EJEMPLO 1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+	
+	int fila, columna;	
+	
+	cout << "Ingrese la cantidad de filas: " << endl;
+	
+	cin >> fila;
+	
+	cout << "Ingrese la cantidad de columnas: " << endl;
+	
+	cin >> columna;
+	
+	//declarada mi matriz
+	
+	int mat[fila][columna];
+	
+	for(int i = 0; i < fila; i++){
+		for(int j = 0; j < columna; j++){
+			
+			cout << "Ingrese el dato de la posicion " << i+1 <<", "<< j+1 << ": " <<endl;
+			
+			cin >> mat[i][j];
+		}
+	}
+	
+	for(int k = 0; k < fila; k++){
+		for(int l = 0; l < columna; l++){
+			
+			cout << mat[k][l] << " ";
+			
+		}	
+		cout << endl;
+	}
+	
+	return 0;
+	getch();
+}
